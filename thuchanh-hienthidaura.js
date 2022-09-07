53021606fbd93db4fa560f484b27384dbad6b498
huynguyenquang5 <huynguyenquang5@gmail.com>
2022-09-07 11:27:34 +0700

    add thuchanh hienthidaura html js

---
+++ thuchanh-hienthidaura.js
@@ -0,0 +1,19 @@
+// Bài tập 1
+// function showMessage() {
+//     alert('Xin chào!');
+// }
+
+// // Bài tập 2
+// function showMessage(){
+//     document.getElementById('messageDiv').innerHTML = 'Xin chào!';
+// }
+
+//Bài tập 3
+// function showMessage(){
+//     document.write('Xin chào!');
+// }
+
+//Bài tập 4
+function showMessage(){
+    console.log('Xin chào!');
+}
